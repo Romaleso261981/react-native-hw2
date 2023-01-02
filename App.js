0085093eab54aba4d78994d0ad68b712dc4bb23d
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-01-02 10:15:55 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,31 +5,31 @@ import Login from "./screens/auth/Login";
 import Register from "./screens/auth/Register";
 import Home from "./screens/Home";
 import { Dimensions } from "react-native";
-// import * as Font from "expo-font";
-// import { AppLoading } from "expo";
-// import { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { useState } from "react";
 
 const AuthStack = createStackNavigator();
 
-// const loadApplication = async () => {
-//   if (fontsLoaded) {
-//     await Font.loadAsync({
-//       "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-//     });
-//   }
-// };
+const loadApplication = async () => {
+  if (fontsLoaded) {
+    await Font.loadAsync({
+      "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    });
+  }
+};
 
 export default () => {
-  // const [iasReady, setIasReady] = useState(false);
-  // if (!iasReady) {
-  //   return (
-  //     <AppLoading
-  //       startAsync={loadApplication}
-  //       onfinish={() => setIasReady(true)}
-  //       onError={console.warn}
-  //     />
-  //   );
-  // }
+  const [iasReady, setIasReady] = useState(false);
+  if (!iasReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onfinish={() => setIasReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <AuthStack.Navigator>
